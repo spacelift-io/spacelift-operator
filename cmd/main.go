ec2268e3cf72af9d62f3ed78b19a73024592c3ba
Michał Goliński <michalg@spacelift.io>
2024-03-01 11:03:44 +0100

    Scaffolding for Stack

--- cmd/main.go
+++ cmd/main.go
@@ -111,6 +111,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Run")
 		os.Exit(1)
 	}
+	if err = (&controller.StackReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Stack")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
