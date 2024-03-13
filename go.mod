ef9044946f70ed7ec17b951e597690b56277dd04
Michał Goliński <3693729+michalg9@users.noreply.github.com>
2024-03-13 12:14:19 +0100

    Update client to handle session removal (#15)
    * Update client to handle session removal
    
    * timer -> now name change
    
    * golint

--- go.mod
+++ go.mod
@@ -11,10 +11,10 @@ require (
 	github.com/oklog/ulid/v2 v2.1.0
 	github.com/pkg/errors v0.9.1
 	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466
-	github.com/spacelift-io/spacectl v0.30.0
 	github.com/stretchr/testify v1.8.4
 	github.com/tmdvs/Go-Emoji-Utils v1.1.0
 	go.uber.org/zap v1.27.0
+	golang.org/x/oauth2 v0.14.0
 	k8s.io/api v0.28.0
 	k8s.io/apimachinery v0.28.0
 	k8s.io/client-go v0.28.0
@@ -34,7 +34,6 @@ require (
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
@@ -53,6 +52,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/onsi/ginkgo/v2 v2.13.0 // indirect
+	github.com/onsi/gomega v1.30.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.16.0 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
@@ -61,9 +61,10 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
 	golang.org/x/net v0.18.0 // indirect
-	golang.org/x/oauth2 v0.14.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/term v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- internal/spacelift/client/client.go
+++ internal/spacelift/client/client.go
@@ -2,13 +2,20 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/shurcooL/graphql"
-	spacectl "github.com/spacelift-io/spacectl/client"
-	"github.com/spacelift-io/spacectl/client/session"
+	"golang.org/x/oauth2"
 	v1 "k8s.io/api/core/v1"
 	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/spacelift-io/spacelift-operator/internal/spacelift/client/session"
 )
 
 // spaceliftClient is the authenticated client that can be used to interact with Spacelift
@@ -39,46 +46,106 @@ func GetSpaceliftClient(ctx context.Context, client k8sclient.Client, namespace 
 		return nil, errors.Wrap(err, "failed to get spacelift credentials secret")
 	}
 
-	endpoint := string(secret.Data[SpaceliftApiKeyEndpointKey])
+	apiEndpoint := string(secret.Data[SpaceliftApiKeyEndpointKey])
 	apiKeyID := string(secret.Data[SpaceliftApiKeyIDKey])
 	apiKeySecret := string(secret.Data[SpaceliftApiKeySecretKey])
 
-	newSpaceliftClient, err := New(endpoint, apiKeyID, apiKeySecret)
+	session, err := func() (session.Session, error) {
+		sessionCtx, cancel := context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		return session.New(sessionCtx, http.DefaultClient, apiEndpoint, apiKeyID, apiKeySecret)
+	}()
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to create spacelift client")
+		return nil, errors.Wrap(err, "could not create session from Spacelift API key")
 	}
 
-	spaceliftClient = newSpaceliftClient
+	spaceliftClient = New(http.DefaultClient, session)
 
 	return spaceliftClient, nil
 }
 
 type client struct {
-	wraps spacectl.Client
+	wraps   *http.Client
+	session session.Session
 }
 
 // New returns a new instance of a Spacelift Client.
-func New(endpoint, keyID, keySecret string) (*client, error) {
-	ctx, httpClient := session.Defaults()
-
-	session, err := session.FromAPIKey(ctx, httpClient)(endpoint, keyID, keySecret)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create spacelift session")
-	}
-
-	spaceliftClient := spacectl.New(httpClient, session)
-
-	return &client{wraps: spaceliftClient}, nil
+func New(wraps *http.Client, session session.Session) Client {
+	return &client{wraps: wraps, session: session}
 }
 
 func (c *client) Mutate(ctx context.Context, mutation interface{}, variables map[string]interface{}, opts ...graphql.RequestOption) error {
-	return c.wraps.Mutate(ctx, mutation, variables, opts...)
+	logger := log.FromContext(ctx)
+
+	apiClient, err := c.apiClient(ctx)
+	if err != nil {
+		return nil
+	}
+
+	err = apiClient.Mutate(ctx, mutation, variables, opts...)
+	if err != nil && strings.Contains(err.Error(), "unauthorized") {
+		logger.Error(err, "Server returned an unauthorized response - retrying request with a new token")
+		c.session.RefreshToken(ctx)
+
+		// Try again in case refreshing the token fixes the problem
+		apiClient, err = c.apiClient(ctx)
+		if err != nil {
+			return err
+		}
+
+		err = apiClient.Mutate(ctx, mutation, variables, opts...)
+	}
+
+	return err
 }
 
 func (c *client) Query(ctx context.Context, query interface{}, variables map[string]interface{}, opts ...graphql.RequestOption) error {
-	return c.wraps.Query(ctx, query, variables, opts...)
+	logger := log.FromContext(ctx)
+
+	apiClient, err := c.apiClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = apiClient.Query(ctx, query, variables)
+	if err != nil && strings.Contains(err.Error(), "unauthorized") {
+		logger.Error(err, "Server returned an unauthorized response - retrying request with a new token")
+		c.session.RefreshToken(ctx)
+
+		// Try again in case refreshing the token fixes the problem
+		apiClient, err = c.apiClient(ctx)
+		if err != nil {
+			return err
+		}
+
+		err = apiClient.Query(ctx, query, variables, opts...)
+	}
+
+	return err
 }
 
 func (c *client) URL(format string, a ...interface{}) string {
-	return c.wraps.URL(format, a...)
+	endpoint := c.session.Endpoint()
+
+	endpointURL, err := url.Parse(endpoint)
+	if err != nil {
+		panic(err) // Impossible condition.
+	}
+
+	endpointURL.Path = fmt.Sprintf(format, a...)
+
+	return endpointURL.String()
+}
+
+func (c *client) apiClient(ctx context.Context) (*graphql.Client, error) {
+	bearerToken, err := c.session.BearerToken(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return graphql.NewClient(c.session.Endpoint(), oauth2.NewClient(
+		context.WithValue(ctx, oauth2.HTTPClient, c.wraps), oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: bearerToken},
+		),
+	)), nil
 }
