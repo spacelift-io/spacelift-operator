54b15d7148f321b81af08640b646a84f07e10d93
peterdeme <snypox@gmail.com>
2025-05-23 16:16:08 +0200

    chore: bump packages
    Signed-off-by: peterdeme <snypox@gmail.com>

--- tools.go
+++ tools.go
@@ -2,7 +2,7 @@
 
 package tools
 
-//go:generate go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.0
-//go:generate go install github.com/daixiang0/gci@v0.13.5
-//go:generate go install github.com/goreleaser/goreleaser/v2@v2.4.4
-//go:generate go install github.com/vektra/mockery/v2@v2.46.3
+//go:generate go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.1.6
+//go:generate go install github.com/daixiang0/gci@v0.13.6
+//go:generate go install github.com/goreleaser/goreleaser/v2@v2.9.0
+//go:generate go install github.com/vektra/mockery/v2@v2.53.4
